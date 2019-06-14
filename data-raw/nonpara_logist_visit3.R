07260828b7b624a6de3a5afca4f08ab7023feb97
Unkyung Lee <uklee1029@gmail.com>
2019-06-13 23:06:56 -0500

    updated

--- data-raw/nonpara_logist_visit3.R
+++
@@ -1,4 +0,0 @@
-load("C:/Users/unkyung/Dropbox/Project_Posdoc_Tanya/HDproject_local/Revision/code_for_R_package/nonpara_logist_visit3.RData")
-nonpara_logist_visit3<-results
-save(nonpara_logist_visit3, file="Data/nonpara_logist_visit3.RData")
-usethis::use_data(nonpara_logist_visit3, overwrite = TRUE)
