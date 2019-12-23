4d17f2606d199ff9c404a00f0b2b68117c180d2d
Unkyung Lee <42941669+unkyunglee@users.noreply.github.com>
2019-12-23 11:53:23 -0600

    update usethis function

--- data-raw/nonpara_logist_visit1_nsim500.R
+++ data-raw/nonpara_logist_visit1_nsim500.R
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
+
 load("C:/Users/unkyung/Dropbox/logist/nonpara/logist_visit1/combined_nsim500_logist_visit1.RData")
 
 nonpara_logist_visit1_nsim500<-combi.res
 save(nonpara_logist_visit1_nsim500, file="Data/nonpara_logist_visit1_nsim500.RData")
 usethis::use_data(nonpara_logist_visit1_nsim500, overwrite = TRUE)
-=======
-load("C:/Users/unkyung/Dropbox/logist/nonpara/logist_visit1/combined_nsim500_logist_visit1.RData")
 
-nonpara_logist_visit1_nsim500<-combi.res
-save(nonpara_logist_visit1_nsim500, file="Data/nonpara_logist_visit1_nsim500.RData")
-usethis::use_data(nonpara_logist_visit1_nsim500, overwrite = TRUE)
->>>>>>> 1e019c4ccae424fd5a5f056475f3487fd881ab67
+
