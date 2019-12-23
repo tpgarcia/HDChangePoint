ccfa260da7bf8357afc835d2ec8c77174bbf00e6
Unkyung Lee <42941669+unkyunglee@users.noreply.github.com>
2019-12-23 10:29:17 -0600

    Delete old simulation results

--- data-raw/nonpara_logist_visit1.R
+++
@@ -1,5 +0,0 @@
-load("C:/Users/unkyunglee/Dropbox/Project_Posdoc_Tanya/HDproject_local/Rcode(Revision_Rpackage)/code_for_R_package/HDChangePoint/Data/nonpara_logist_visit1.RData")
-
-nonpara_logist_visit1<-results
-save(nonpara_logist_visit1, file="Data/nonpara_logist_visit1.RData")
-usethis::use_data(nonpara_logist_visit1, overwrite = TRUE)
